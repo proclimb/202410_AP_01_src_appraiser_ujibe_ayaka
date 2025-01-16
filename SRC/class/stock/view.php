b7b61522bc2bcd0f3538062bd9cc8a4a35c29c7c
a.ujibe <202410ap01_ujibe@x-ide.sakura.ne.jp>
2025-01-16 12:19:26 +0900

    #32136　仕入登録画面の初期表示で距離項目の「A（5分以内）」が選択されていない

--- SRC/class/stock/view.php
+++ SRC/class/stock/view.php
@@ -294,7 +294,12 @@ function subStockEditView($param)
 					<?php
 					for ($i = 0; $i < 4; $i++) {
 					?>
-						<input type="radio" name="distance" value="<?php print $i + 1; ?>" <?php if ($param["distance"] == $i + 1) print ' checked="checked"'; ?> /> <?php print fnDistanceName($i); ?>
+						<input type="radio" name="distance" value="<?php print $i + 1; ?>"
+							<?php if ($param["distance"] == $i + 1) {
+								print " checked";
+							} else if ($param["distance"] == "" && $i == 0) {
+								print " checked";
+							} ?> /> <?php print fnDistanceName($i); ?>
 					<?php
 					}
 					?>
