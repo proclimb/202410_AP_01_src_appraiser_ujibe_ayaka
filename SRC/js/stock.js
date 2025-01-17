e4bd027117cb9854530accfa6ae95c15f3288ee2
a.ujibe <202410ap01_ujibe@x-ide.sakura.ne.jp>
2025-01-17 14:19:43 +0900

    #33797　エラーメッセージが表示されず、登録処理が走る

--- SRC/js/stock.js
+++ SRC/js/stock.js
@@ -31,7 +31,7 @@ function fnStockEditCheck() {
 	}
 
 	tmp = form.area.value;
-	if (tmp.length > 6 || tmp.match(/[^0-9\.]+/)) {
+	if (tmp >= 1000 || tmp.length > 6 || tmp.match(/[^0-9\.]+/)) {
 		alert('面積は3桁以内（小数点以下2桁以内）の半角数字で入力してください');
 		return;
 	}
