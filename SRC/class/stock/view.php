1e83f768a69d1f97b1ddf5d53f62a3677e51d717
a.ujibe <202410ap01_ujibe@x-ide.sakura.ne.jp>
2025-01-21 11:51:46 +0900

    軽微な修正

--- SRC/class/stock/view.php
+++ SRC/class/stock/view.php
@@ -354,7 +354,7 @@ function subStockEditView($param)
 
 		</table>
 
-		<a href="javascript:fnStockEditCheck();"><img src="./images/<?php print $param["btnImage"] ?>" /></a>　
+		<a href="javascript:fnStockEditCheck();"><img src="./images/<?php print $param["btnImage"] ?>" /></a>
 		<a href="javascript:form.act.value='stockSearch';form.submit();"><img src="./images/btn_return.png" /></a>
 		<?php
 		if ($param["stockNo"]) {
